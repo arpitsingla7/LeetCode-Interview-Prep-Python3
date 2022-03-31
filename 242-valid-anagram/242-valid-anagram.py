5cce2cce13572760c6e77336ebc171a7909b1494
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-03-31 13:23:06 -0400

    Time: 59 ms (69.52%), Space: 14.5 MB (69.67%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -2,13 +2,10 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
         charSet = set(s)
-        count = 0
         for i in charSet:
             if s.count(i)!=t.count(i):
                 return False
-            else:
-                count+=s.count(i)
             
-        if count==len(t):
+        if len(s)==len(t):
             return True
         return False
