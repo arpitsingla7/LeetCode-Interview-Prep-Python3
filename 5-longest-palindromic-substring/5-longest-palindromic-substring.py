305a8a14355f0a5bad60d86eb8e7b7b2fbe60ddb
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-03-31 22:28:31 -0400

    Time: 1172 ms (65.03%), Space: 13.9 MB (63.68%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,28 +1,44 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         
-        res = s[0]
-        resLen = 0
+        self.res = s[0]
+        self.resLen = 0
+        def findres(l, r):
+            while (l>=0 and r<len(s)) and (s[l] == s[r]):
+                if (r-l+1)>self.resLen:
+                    self.res = s[l:r+1]
+                    self.resLen = r-l+1
+                l-=1
+                r+=1
+            
+            return self.res
+        
+        
         for c in range(len(s)):
             
-            # if (c-1>=0 and c+1<len(s)) and (s[c-1] == s[c+1]):
-            l = c-1
-            r = c+1
-            while (l>=0 and r<len(s)) and (s[l] == s[r]):
-                if (r-l+1)>resLen:
-                    res = s[l:r+1]
-                    resLen = r-l+1
-                l-=1
-                r+=1
+            odd = findres(c-1, c+1)
+            even = findres(c, c+1)
             
-            # elif (c+1<=len(s)-1) and (s[c]==s[c+1]):
-            l = c
-            r = c+1
-            while (l>=0 and r<len(s)) and (s[l] == s[r]):
-                if (r-l+1)>resLen:
-                    res = s[l:r+1]
-                    resLen = r-l+1
-                l-=1
-                r+=1
+            res = odd if len(odd)>len(even) else even
+            
+#             l = c-1
+#             r = c+1
+#             while (l>=0 and r<len(s)) and (s[l] == s[r]):
+#                 if (r-l+1)>resLen:
+#                     res = s[l:r+1]
+#                     resLen = r-l+1
+#                 l-=1
+#                 r+=1
+            
+#             l = c
+#             r = c+1
+#             while (l>=0 and r<len(s)) and (s[l] == s[r]):
+#                 if (r-l+1)>resLen:
+#                     res = s[l:r+1]
+#                     resLen = r-l+1
+#                 l-=1
+#                 r+=1
+
+            
         
         return res
