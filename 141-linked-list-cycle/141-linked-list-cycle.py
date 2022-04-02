06fd590e523092ef2bb8b1590936eb52fee33aaf
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-02 17:20:17 -0400

    Time: 72 ms (61.54%), Space: 18 MB (10.69%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -7,9 +7,6 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         
-        if not head:
-            return False
-        
         nodeSet = set()
         node = head
         while node:
