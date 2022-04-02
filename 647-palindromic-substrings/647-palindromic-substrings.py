bc9ae47bd5472d921b1d6218d2a93acbd53b7815
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-02 14:50:57 -0400

    Time: 173 ms (62.30%), Space: 13.9 MB (80.07%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,32 +1,23 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         
-        #odd case
-        res = s[0]
-        resLen = 0
+        
         count = len(s)
         for c in range(len(s)):
+            #odd case
             l = c-1
             r = c+1
             while l>=0 and r<len(s) and s[l]==s[r]:
                 count+=1
-                if resLen<(r-l+1):
-                    res = s[l:r+1]
-                    resLen = r-l+1
-                    
                 l-=1
                 r+=1
             
+            #even case
             l = c
             r = c+1
             while l>=0 and r<len(s) and s[l]==s[r]:
                 count+=1
-                if resLen<(r-l+1):
-                    res = s[l:r+1]
-                    resLen = r-l+1
-                    
                 l-=1
                 r+=1
             
-        print(count)
         return count
