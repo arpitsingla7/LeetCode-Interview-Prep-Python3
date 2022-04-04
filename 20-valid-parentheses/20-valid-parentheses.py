b858f9b2864797f9c8875e5093e02566d3711bd5
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-04 19:43:04 -0400

    Time: 36 ms (76.51%), Space: 13.8 MB (74.55%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,17 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         
-        mapper = {")":"(", "]":"[", "}":"{"}
+        mapper = {")": "(" ,"]" : "[", "}": "{"}
         stack = []
         
-        for bracket in s:
-            if bracket not in mapper:
-                stack.append(bracket)
-                continue
-            if stack and stack[-1]==mapper[bracket]:
-                stack.pop()
+        for b in s:
+            if b not in mapper:
+                stack.append(b)
             else:
-                return False
-        
-        return len(stack)==0
-                
+                if stack and stack[-1]==mapper[b]:
+                    stack.pop()
+                else:
+                    return False
+            
+        return True if len(stack)==0 else False
