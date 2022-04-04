a658236a617bd736c8db3451fd1f259bde5a4b4c
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-04 17:50:21 -0400

    Time: 132 ms (67.59%), Space: 17.5 MB (95.25%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -6,86 +6,43 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         
-        if len(lists)==0 or not lists:
-                return None
         
+        if not lists or len(lists)==0:
+            return None
         
-        while len(lists)>1:
+        while len(lists)!=1:
             mergedLists = []
-            for i in range(0, len(lists), 2): 
-                l1 = lists[i]
-                l2 = lists[i+1] if (i+1)<len(lists) else None
-                mergedLists.append(self.merge(l1, l2))
+            for l in range(0, len(lists), 2):
+                l1 = lists[l]
+                l2 = lists[l+1] if (l+1)<len(lists) else None
+                mergedLists.append(self.mergeSort(l1, l2))
 
             lists = mergedLists
-
         return lists[0]
-            
-
-            
         
-    #merge sort
-    def merge(self, list1, list2):
+        
+        
+        
+    def mergeSort(self, l1, l2):
 
         dummy = ListNode()
         tail = dummy
 
-        while list1 and list2:
-            if list1.val<list2.val:
-                tail.next = list1
-                list1 = list1.next
+        while l1 and l2:
+
+            if l1.val<l2.val:
+                tail.next = l1
+                l1 = l1.next
 
             else:
-                tail.next = list2
-                list2 = list2.next
+                tail.next = l2
+                l2 = l2.next
 
             tail = tail.next
 
-        if list1:
-            tail.next = list1
-        elif list2:
-            tail.next = list2
+        if l1:
+            tail.next = l1
+        elif l2:
+            tail.next = l2
 
         return dummy.next
-            
-        
-    
-        
-        
-        
-        
-        
-#         #brute force solution
-#         if len(lists)==0:
-#             return None
-        
-#         if len(lists)==1:
-#             return lists[0]
-        
-#         while len(lists)!=1:
-#             dummy = ListNode()
-#             tail = dummy
-#             list1 = lists[0]
-#             list2 = lists[1]
-            
-#             while list1 and list2:
-#                 if list1.val<list2.val:
-#                     tail.next = list1
-#                     list1 = list1.next
-                
-#                 else:
-#                     tail.next = list2
-#                     list2 = list2.next
-                    
-#                 tail = tail.next
-            
-#             if list1:
-#                 tail.next = list1
-#             elif list2:
-#                 tail.next = list2
-            
-#             lists.pop(0)
-#             lists.pop(0)
-#             lists.insert(0, dummy.next)
-            
-#         return lists[0]
