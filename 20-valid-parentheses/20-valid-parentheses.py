8de2461610d5d1fee36be0d5350235d8b8916b6f
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-06 17:53:26 -0400

    Time: 46 ms (48.15%), Space: 13.9 MB (27.49%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,16 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         
-        mapper = {")": "(" ,"]" : "[", "}": "{"}
+        closeMap = {")": "(", "]":"[", "}":"{"}
         stack = []
         
-        for b in s:
-            if b not in mapper:
-                stack.append(b)
+        for bracket in s:
+            
+            if bracket not in closeMap:
+                stack.append(bracket)
             else:
-                if stack and stack[-1]==mapper[b]:
-                    stack.pop()
+                if stack and stack[-1] == closeMap[bracket]:
+                    stack.pop() 
                 else:
                     return False
-            
+                
         return True if len(stack)==0 else False
