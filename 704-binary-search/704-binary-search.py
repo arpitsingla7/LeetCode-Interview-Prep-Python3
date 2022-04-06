af41a66d2dee6793f9f89bd13019fb3b01b4c1e5
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-06 18:42:38 -0400

    Time: 406 ms (20.45%), Space: 15.4 MB (76.56%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -2,8 +2,8 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
         
-        l, r = 0, len(nums)
-        while l<r:
+        l, r = 0, len(nums)-1
+        while l<=r:
             m = l + (r-l)//2
             
             if nums[m]==target:
@@ -13,7 +13,7 @@ class Solution:
                 l = m+1
             
             else:
-                r = m
+                r = m-1
         
         return -1
                 
