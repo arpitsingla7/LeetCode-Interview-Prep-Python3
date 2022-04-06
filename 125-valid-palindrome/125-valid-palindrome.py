bca07e12973492e15af1494f28ccfdbb36198711
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-06 16:45:27 -0400

    Time: 82 ms (29.78%), Space: 14.5 MB (47.74%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -2,19 +2,32 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         
         
-        sPali = ""
         
-        for i in s:
-            if i.isalnum():
-                sPali+=i.lower()
         
-        l, r = 0, len(sPali)-1
+        l, r = 0, len(s)-1
         
         while l<r:
-            if sPali[l]!=sPali[r]:
+            while l<r and not self.isalpha(s[l]):
+                l+=1
+            
+            while l<r and not self.isalpha(s[r]):
+                r-=1
+                
+            if s[l].lower() != s[r].lower():
                 return False
             l+=1
             r-=1
+            
         return True
+    
+        
+    def isalpha(self, letter):
+        if ((ord("A")<=ord(letter) and ord(letter)<=ord("Z")) 
+           or (ord("a")<=ord(letter) and ord(letter)<=ord("z"))
+           or (ord("0")<=ord(letter) and ord(letter)<=ord("9"))):
+            return True
+        return False
+        
+            
         
         
