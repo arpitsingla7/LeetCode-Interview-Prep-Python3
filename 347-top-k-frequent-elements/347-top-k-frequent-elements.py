a0d9524d7bfdc32b5d3d771711d4fffe4bb132ba
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-05 22:46:16 -0400

    Time: 142 ms (50.60%), Space: 19.5 MB (15.75%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -5,10 +5,7 @@ class Solution:
         count = {}
         
         for i in nums:
-            if i in count:
-                count[i]+=1
-                continue
-            count[i] = 1
+            count[i] = 1 + count.get(i, 0)
             
         
         res = []
@@ -17,12 +14,11 @@ class Solution:
         for i, val in count.items():
             freq[val].append(i)
             
-        print(freq)
+        
         for i in range(len(freq)-1, 0, -1):
             if len(freq[i])!=0:
-                
                 res.extend(freq[i])
-
+                
                 if len(res)==k:
                     return res
         
