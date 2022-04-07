a5d908324b2313de5b44b515cd52b0233314e0b4
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-06 22:00:02 -0400

    Time: 48 ms (38.30%), Space: 14 MB (33.78%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -8,17 +8,37 @@ class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         
         
-        #recursive method:
-        
-        def dfs(p, q):
-            if not p and not q:
-                return True
+        #bfs method
+        if not p and not q:
+            return True
+        elif (not p and q) or (p and not q):
+            return False
             
-            if (not p and q) or (p and not q):
+        queue = [[p, q]]
+        
+        while queue:
+            
+            p, q = queue.pop(0)
+            
+            if p and q:
+                if p.val != q.val:
+                    return False
+                queue.append([p.left, q.left])
+                queue.append([p.right, q.right])
+            
+            elif p or q:
                 return False
+                    
+        return True
             
-            balance = (p.val == q.val) and dfs(p.left, q.left) and dfs(p.right, q.right)
             
-            return balance
-        
-        return dfs(p, q)
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
