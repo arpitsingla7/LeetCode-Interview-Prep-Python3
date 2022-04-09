2f5170e1ca3a0d3fe6f54752e9eca9f3410ca606
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-09 15:47:21 -0400

    Time: 36 ms (98.38%), Space: 14.2 MB (59.72%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -6,40 +6,40 @@ class Solution:
         
         while l<=r:
             
-            #array is sorted
-            if nums[l]<nums[r]:
-                #run binary search
-                while l<=r:
-                    print("in")
-                    m = l+(r-l)//2
+#             #array is sorted
+#             if nums[l]<nums[r]:
+#                 #run binary search
+#                 while l<=r:
+#                     print("in")
+#                     m = l+(r-l)//2
                 
-                    if target==nums[m]:
-                        return m
-                    elif target>nums[m]:
-                        l=m+1
+#                     if target==nums[m]:
+#                         return m
+#                     elif target>nums[m]:
+#                         l=m+1
 
-                    elif target<nums[m]:
-                        r = m-1
+#                     elif target<nums[m]:
+#                         r = m-1
                 
-                return -1
+#                 return -1
             
-            else:
+#             else:
                 
-                m = l+(r-l)//2
-                if target==nums[m]:
-                    return m
+            m = l+(r-l)//2
+            if target==nums[m]:
+                return m
 
-                if nums[l]<=nums[m]:
-                    if target<nums[l] or target>nums[m]:
-                        l=m+1
-                    else:
-                        r=m-1
-                
+            if nums[l]<=nums[m]:
+                if target<nums[l] or target>nums[m]:
+                    l=m+1
                 else:
-                    if target>nums[r] or target<nums[m]:
-                        r = m-1
-                    
-                    else:
-                        l = m+1 
-        
+                    r=m-1
+
+            else:
+                if target>nums[r] or target<nums[m]:
+                    r = m-1
+
+                else:
+                    l = m+1 
+
         return -1
