8dad90bd02aa10a48717c82e9187b6eed0b27f3e
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-08 23:53:54 -0400

    Time: 107 ms (64.21%), Space: 14.1 MB (35.70%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -3,26 +3,29 @@ class Solution:
         
         if len(s1)>len(s2):
             return False
-        s1Count = {}
-        s2Count = {}
-        for i in range(len(s1)):
-            s1Count[s1[i]] = s1Count.get(s1[i], 0) + 1
-            s2Count[s2[i]] = s2Count.get(s2[i], 0) + 1
         
-        if s1Count == s2Count:
+        s1Map = [0]*26
+        s2Map = [0]*26
+        for i in range(len(s1)):
+            s1Map[ord(s1[i])-ord("a")] +=1
+            s2Map[ord(s2[i])-ord("a")] +=1
+        
+        if s2Map == s1Map:
             return True
         
-        l = 0
+        l=0
         for r in range(len(s1), len(s2)):
-            s2Count[s2[l]]-=1
-            s2Count[s2[r]] = s2Count.get(s2[r], 0) + 1
-            
-            if s2Count[s2[l]] == 0:
-                s2Count.pop(s2[l])
-            
+            s2Map[ord(s2[l])-ord("a")]-=1
             l+=1
-            if s2Count == s1Count:
+            
+            s2Map[ord(s2[r])-ord("a")]+=1
+            
+            if s2Map==s1Map:
                 return True
         
         return False
+            
+        
+        
+            
             
