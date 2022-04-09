f759fef6743f1cadeeabd8a8cbba0d80c064cc6d
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-09 19:57:22 -0400

    Time: 142 ms (32.94%), Space: 24.2 MB (31.79%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -10,43 +10,41 @@ class Solution:
         """
         
         
-        slow, fast = head, head
+        
+        slow, fast = head, head.next
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
             
-        
-        #reverse the 2nd half of the list
         second = slow.next
-        prev = slow.next = None
+        prev = None
         while second:
             nxt = second.next
             second.next = prev
             prev = second
             second = nxt
         
-        #then use the first half and second half to make the final list
-        first = head
         second = prev
+        first = head
+        slow.next = None
+        
+        
         while second:
-            temp1, temp2 = first.next, second.next
+            node1 = first.next
+            node2 = second.next
+            
             first.next = second
-            second.next = temp1
-            first = temp1
-            second = temp2
+            second.next = node1
+            first = node1
+            second = node2
+        
+        return head
         
         
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+        
+        
+        
+        
+        
+        
+        
