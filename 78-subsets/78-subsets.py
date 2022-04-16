b0a3f86826da9100f79623b8dc420b2b43edfe52
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-16 17:35:16 -0400

    Time: 25 ms (99.13%), Space: 14.1 MB (82.23%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,20 +1,24 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         
+        
         res = []
         subset = []
-        def backTrack(i):
-            if i>=len(nums):
-                res.append(subset.copy())
-                return 
-            
-            #including the no 
-            subset.append(nums[i])
-            backTrack(i+1)
-            
-            #not including
-            subset.pop()
-            backTrack(i+1)
         
-        backTrack(0)
+        def dfs(i):
+            if i==len(nums):
+                res.append(subset.copy())
+                return
+            
+            #include
+            subset.append(nums[i])
+            dfs(i+1)
+            
+            
+            #not include
+            subset.pop()
+            dfs(i+1)
+            
+        
+        dfs(0)
         return res
