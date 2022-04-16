dcc6eeca60122108b79287cf75a956e6bc1ec97a
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-16 15:06:39 -0400

    Time: 65 ms (77.28%), Space: 15.4 MB (35.92%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.py
+++ 784-letter-case-permutation/784-letter-case-permutation.py
@@ -10,106 +10,24 @@ class Solution:
                 res.append("".join(copy))
                 return 
             
-            substr.append(s[i].lower())
-            dfs(i+1)
-            substr.pop()
+            
             if s[i].isalpha():
+                substr.append(s[i].lower())
+                dfs(i+1)
+                substr.pop()
                 
                 substr.append(s[i].upper())
                 dfs(i+1)
                 substr.pop()
-            # else:
-            #     substr.append(s[i])
-            #     dfs(i+1)
-            #     substr.pop()
+            else:
+                substr.append(s[i])
+                dfs(i+1)
+                substr.pop()
             
         
         dfs(0)
         return res
         
         
-        
-        
-        
-        
-        
-                
-            
-        
-#         result = []
-#         partial = []
-#         S = s
-#         def permHelper(index, partial):
-            
-#             if index==len(S):
-#                 result.append(''.join(partial))
-#                 return
-            
-#             if S[index].isalpha():
-#                 partial.append(S[index].lower())
-#                 permHelper(index+1, partial)
-#                 partial.pop()
-#                 partial.append(S[index].upper())
-#                 permHelper(index+1, partial)
-#                 partial.pop()
-                
-#             else:
-#                 partial.append(S[index])
-#                 permHelper(index+1, partial)
-#                 partial.pop()
 
-#         permHelper(0, partial)
-#         return result
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         res = []
-#         substr = []
-#         s = s.lower()
-#         def backTrack(i):
-#             if i>=len(s):
-#                 res.append("".join(substr.copy()))
-#                 return 
-            
-#             #lower case
-#             substr.append(s[i])
-#             if s[i].isalpha():
-#                 backTrack(i+1)
-#                 substr.pop()
-                
-#                 #upper case
-#                 substr.pop()
-#                 substr.append(s[i].upper())
-                
-#             backTrack(i+1)
-        
-#         backTrack(0)
-#         print(res)
-                
-            
-            
-            
+    
