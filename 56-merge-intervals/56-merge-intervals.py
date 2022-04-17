71a09c24b0ef47024122fa65c1f8d8b2bc8c96b7
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-17 15:40:41 -0400

    Time: 148 ms (93.59%), Space: 18 MB (98.92%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -6,7 +6,6 @@ class Solution:
         for i in range(len(intervals)-1):
             if intervals[i][1]<intervals[i+1][0]:
                 res.append(intervals[i])
-                pass
             else:
                 intervals[i+1] = [intervals[i][0], max(intervals[i][1], intervals[i+1][1])]
                 
