ee5668c1dc7cb7b265125ee6703a1c434565e103
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-18 19:38:28 -0400

    Time: 476 ms (95.46%), Space: 15.1 MB (97.26%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -3,9 +3,10 @@ class Solution:
         
         goal = len(nums)-1
         
-        for i in range(len(nums)-2, -1, -1):
+        for i in range(len(nums)-1, -1, -1):
             if i+nums[i]>=goal:
                 goal = i
+                
         
         return goal==0
             
