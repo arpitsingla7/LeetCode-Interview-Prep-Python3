d057aed2d7b5323f115926023b2c489af06b5e8a
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-21 19:06:00 -0400

    Time: 45 ms (47.23%), Space: 13.9 MB (69.51%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,31 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        if len(nums)<=2:
-            return max(nums)
+        rob1, rob2 = 0, 0
         
-        elif len(nums)==3:
-            return max(nums[0]+nums[2], nums[1])
-        
-        cache = {}
-        
-        def dfs(ind, curRes):
-            if ind in cache:
-                return cache[ind]
-            
-            if ind>=len(nums):
-                return 
-            
-            curRes+=nums[ind]
-            dfs(ind+2, curRes)
-            cache[ind] = max(cache.get(ind+2, 0)+nums[ind], cache.get(ind+3, 0)+nums[ind], nums[ind])
-            
-            curRes-=nums[ind]
-            dfs(ind+3, curRes)
-            cache[ind] = max(cache.get(ind+2, 0)+nums[ind], cache.get(ind+3, 0)+nums[ind], nums[ind])
-        
-        dfs(0, 0)
-        dfs(1, 0)
-        return max(cache[0], cache[1])
+        for i in nums:
+            temp = max(rob1+i, rob2)
+            rob1 = rob2
+            rob2 = temp
             
         
+        return rob2
