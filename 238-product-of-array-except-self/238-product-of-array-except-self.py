54665dc7ea88d138765f0c07b86d1366321a2d69
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-23 14:45:32 -0400

    Time: 268 ms (65.56%), Space: 21 MB (83.32%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -10,12 +10,9 @@ class Solution:
         for i in range(1, len(nums)):
             prefix[i] = prefix[i-1]*nums[i-1]
         
-        suffix = [1]*len(nums)
-        
+        suffix = 1
         for i in range(len(nums)-2, -1, -1):
-            suffix[i] = suffix[i+1]*nums[i+1]
-        
-        for i in range(len(nums)):
-            prefix[i] = prefix[i]*suffix[i]
+            suffix = suffix*nums[i+1]
+            prefix[i] = suffix*prefix[i]
         
         return prefix
