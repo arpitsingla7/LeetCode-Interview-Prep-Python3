b7fe03069942705971653b759545341a4a545698
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-23 13:13:16 -0400

    Time: 211 ms (12.42%), Space: 20.1 MB (8.85%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,30 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        #method 2:
+        
+        #check solution for capital letters 
+        
         res = defaultdict(list)
         
         for word in strs:
-            count = [0]*26
-            for c in word:
-                count[ord(c)-ord("a")] +=1
-            res[tuple(count)].append(word)
+            letters = [0]*26
+            for ch in word:
+                letters[ord(ch)-ord("a")] += 1
+            
+            res[tuple(letters)].append(word)
         
         return res.values()
-        
-        
-        
-        #method 1:
-        res = defaultdict(list)
-        
-        for word in strs:
-            sWord = sorted(word)
-            sWord = "".join(sWord)
-            if sWord in res:
-                res[sWord].append(word)
-                continue
-            
-            res[sWord].append(word)
-            
-        return res.values()
-        
