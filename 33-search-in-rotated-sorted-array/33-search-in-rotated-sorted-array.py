20f23db87feb512637b2f28fcb1cf428c18304c7
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-24 12:52:15 -0400

    Time: 65 ms (33.99%), Space: 14.2 MB (92.25%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -5,41 +5,21 @@ class Solution:
         l, r = 0, len(nums)-1
         
         while l<=r:
+            m = l + (r-l)//2
             
-#             #array is sorted
-#             if nums[l]<nums[r]:
-#                 #run binary search
-#                 while l<=r:
-#                     print("in")
-#                     m = l+(r-l)//2
-                
-#                     if target==nums[m]:
-#                         return m
-#                     elif target>nums[m]:
-#                         l=m+1
-
-#                     elif target<nums[m]:
-#                         r = m-1
-                
-#                 return -1
-            
-#             else:
-                
-            m = l+(r-l)//2
             if target==nums[m]:
                 return m
-
-            if nums[l]<=nums[m]:
-                if target<nums[l] or target>nums[m]:
-                    l=m+1
-                else:
-                    r=m-1
-
-            else:
-                if target>nums[r] or target<nums[m]:
+            
+            if nums[m]<=nums[r]:
+                if target<nums[m] or target>nums[r]:
                     r = m-1
-
                 else:
-                    l = m+1 
-
+                    l = m+1
+            else:
+                if target>nums[m] or target<nums[l]:
+                    l = m+1
+                else:
+                    r = m-1
+                
+        
         return -1
