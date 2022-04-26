90cb633966b9bfd3d593fa930ca11e75089bff3d
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-04-26 15:08:25 -0400

    Time: 1079 ms (51.76%), Space: 76.6 MB (20.78%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,56 +1,51 @@
-class Node:
+class ListNode:
     def __init__(self, key, val):
-        self.val = val
         self.key = key
+        self.val = val
         self.nxt = None
         self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
         self.cache = {}
-        self.left, self.right = Node(0, 0), Node(0, 0)
-        
-        self.left.nxt = self.right
-        self.right.prev = self.left
-        
+        self.capacity = capacity
+        self.left, self.right = ListNode(0, 0), ListNode(0, 0)
+        self.left.nxt, self.right.prev = self.right, self.left
 
-    def remove(self, node):
-        prev, nxt = node.prev, node.nxt
-        nxt.prev, prev.nxt = prev, nxt
-    
-    def add(self, node):
+        
+    def addNode(self, node):
+        #add node to linked list
+        #add to the right always 
         prev, nxt = self.right.prev, self.right
         node.prev, node.nxt = prev, nxt
-        prev.nxt = nxt.prev = node
+        prev.nxt, nxt.prev = node, node
+    
+    def removeNode(self, node):
+        #remove node from linked list
+        prev, nxt = node.prev, node.nxt
+        prev.nxt, nxt.prev = nxt, prev
         
         
     def get(self, key: int) -> int:
         if key in self.cache:
-            #update the most recently used key in cache
-            #remove from where the node is 
-            self.remove(self.cache[key])
-            #add the node to righmost point
-            self.add(self.cache[key])
-            
+            self.removeNode(self.cache[key])
+            self.addNode(self.cache[key])
             return self.cache[key].val
         return -1
-        
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            #do we need to update the cache 
-            self.remove(self.cache[key])
+            self.removeNode(self.cache[key])
         
-        self.cache[key] = Node(key, value)
-        self.add(self.cache[key])
+        self.cache[key] = ListNode(key, value)
+        self.addNode(self.cache[key])
         
-        
-        if self.capacity<len(self.cache):
+        if len(self.cache)>self.capacity:
             lru = self.left.nxt
-            self.remove(lru)
+            self.removeNode(self.cache[lru.key])
             del self.cache[lru.key]
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
