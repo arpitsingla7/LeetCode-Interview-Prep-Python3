49a3e6c7ba4d3e9edee2340193ebdade273cdedb
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-05-01 17:21:10 -0400

    Time: 110 ms (41.01%), Space: 14.6 MB (12.83%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.py
@@ -0,0 +1,26 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        
+        start = [(i[1], i[0]) for i in trips]
+        start.sort(key = lambda i: i[0])
+        print(start)
+        end = [(i[2], i[0]) for i in trips]
+        end.sort(key = lambda i: i[0])
+        print(end)
+        
+        
+        s, e = 0, 0
+        res, count = 0, 0
+        
+        while s<len(start):
+            if start[s][0]<end[e][0]:
+                count+=start[s][1]
+                s+=1
+                
+            else:
+                count-=end[e][1]
+                e+=1
+            
+            res = max(res, count)
+        
+        return True if res<=capacity else False
