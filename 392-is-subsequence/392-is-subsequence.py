a598e57384608171fe39479033213d2d5e68be2b
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-05-02 15:47:03 -0400

    Time: 45 ms (52.80%), Space: 14 MB (17.91%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -4,10 +4,9 @@ class Solution:
         i, j = 0, 0
         
         while i<len(s) and j<len(t):
-            
             if s[i]==t[j]:
                 i+=1
             j+=1
         
-        return i==len(s)
+        return True if i==len(s) else False
         
