ef67c852fbe06a04a5a64f2a401f1ba3ff27f571
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-05-02 10:15:00 -0400

    Time: 53 ms (41.74%), Space: 14.3 MB (13.13%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,20 +1,49 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         
-        res = []
         
-        def backTrack(remOpenBrackets, curOpenBrackets, s):
-            if len(s)==(n*2):
-                res.append(s)
+        res = []
+        self.s = ""
+        def backTrack(remOpenBrackets, curOpenBrackets):
+            if len(self.s)==(n*2):
+                res.append(self.s)
                 return 
             
             #if we can still add open brackets:
             if remOpenBrackets>0:
-                backTrack(remOpenBrackets-1, curOpenBrackets+1, s+"(")
-            
+                self.s+="("
+                backTrack(remOpenBrackets-1, curOpenBrackets+1)
+                self.s = self.s[:-1]
+            #if we have open brackets that need closing brackets to complete 
             if curOpenBrackets>0:
-                backTrack(remOpenBrackets, curOpenBrackets-1, s+")")
+                self.s+=")"
+                backTrack(remOpenBrackets, curOpenBrackets-1)
+                self.s = self.s[:-1]
+    
+        backTrack(n, 0)
+        return res
+        
+        
+        
+        
+        
+        
+        
+#         res = []
+        
+#         def backTrack(remOpenBrackets, curOpenBrackets, s):
+#             if len(s)==(n*2):
+#                 res.append(s)
+#                 return 
+            
+#             #if we can still add open brackets:
+#             if remOpenBrackets>0:
+#                 backTrack(remOpenBrackets-1, curOpenBrackets+1, s+"(")
+            
+#             #if we have open brackets that need closing brackets to complete 
+#             if curOpenBrackets>0:
+#                 backTrack(remOpenBrackets, curOpenBrackets-1, s+")")
                 
     
-        backTrack(n, 0, "")
-        return res
+#         backTrack(n, 0, "")
+#         return res
