af2be75bcccd21512e7803491d6365ff0e7b73c3
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-05-04 20:37:43 -0400

    Time: 126 ms (83.04%), Space: 15.3 MB (34.54%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,28 +1,32 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         
-        if len(p)>len(s):
+        if len(s)<len(p):
             return []
         
-        sCount, pCount = {}, {}
+        truth = [0]*26
+        for ch in p:
+            truth[ord(ch)-ord("a")] += 1
         
-        for i in range(len(p)):
-            pCount[p[i]] = pCount.get(p[i], 0) + 1
-            sCount[s[i]] = sCount.get(s[i], 0) + 1
+        newTruth = [0]*26
+        for ch in range(len(p)):
+            newTruth[ord(s[ch])-ord("a")]+=1
         
-        res = [0] if sCount == pCount else []
-        l = 0
         
-        for r in range(len(p), len(s)):
-            sCount[s[l]] -= 1
-            sCount[s[r]] = sCount.get(s[r], 0) + 1
+        l, r = 0, len(p)
+        res = []
+        
+        while r<=len(s):
             
-            if sCount[s[l]]==0:
-                sCount.pop(s[l])
+            if newTruth==truth:
+                res.append(l)
+            
+            if r<len(s):
+                newTruth[ord(s[r])-ord("a")]+=1
+                newTruth[ord(s[l])-ord("a")]-=1
             
             l+=1
-            if sCount == pCount:
-                res.append(l)
+            r+=1
         
         return res
         
