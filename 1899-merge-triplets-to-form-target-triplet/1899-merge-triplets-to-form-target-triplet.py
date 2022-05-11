16f1a15b247292e5bb5aeec0cac27f636d9528a0
arpitsingla7 <43362340+arpitsingla7@users.noreply.github.com>
2022-05-10 21:03:07 -0400

    Time: 2248 ms (73.10%), Space: 59.2 MB (38.30%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -2,12 +2,10 @@ class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
         
         a, b, c = target[0], target[1], target[2]
-        p = 0
         check = [0, 0, 0]
         for ind, t in enumerate(triplets):
             x, y, z = t[0], t[1], t[2]
             if (x<=a and y<=b and z<=c):
-                p+=1
             
                 if x==a:
                     check[0] = 1
@@ -21,10 +19,7 @@ class Solution:
             if sum(check)==3:
                 return True
             
-        
         return False
-        
-        # return True
         
     
                 
